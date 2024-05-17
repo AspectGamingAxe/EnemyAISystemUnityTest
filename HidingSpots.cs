be355e0fe5e4f8bdf253339409461b5859be7bf7
Aspect <45709496+AspectGamingAxe@users.noreply.github.com>
2024-05-17 16:06:51 +0100

    Update HidingSpots.cs

--- HidingSpots.cs
+++ HidingSpots.cs
@@ -14,45 +14,44 @@ public class HidingSpots : MonoBehaviour
 
 
 	[Header("Valid Check")]
-	[SerializeField] float RayDistance = 2f; 
-	[SerializeField] bool inrange;
-	[SerializeField] bool inrangecd = true;
-	public float sphereradius = 10f;
-	[SerializeField] RaycastHit? lasthit;
-	[SerializeField] RaycastHit hit;
-	public LayerMask CoverMask;
+	[SerializeField] float RayDistance = 2f; //This changes the distance of the raycast
+	[SerializeField] bool inrange; //Checks if the player is within range of the sphereoverlay
+	[SerializeField] bool inrangecd = true; //This is created for an TurnOffRadius Ienumerator so it is not called every frame
+	public float sphereradius = 10f; //This changes the radius of the sphereoverlay
+	[SerializeField] RaycastHit? lasthit; //This stores the gameobject that is currently being hit if it is cover
+	[SerializeField] RaycastHit hit; ///This stores the gameobject that is currently being hit
+	public LayerMask CoverMask; // LayerMask of your cover, for the Raycast.
 
 	[Header("Refrences")]
-	public Transform playerloc;
-	public Manager manager;
-	public GameObject Player;
+	public Transform playerloc; //Refrence to the player location
+	public Manager manager; //Refrence to the Manager Script
+	public GameObject Player; //Refrence to the players gameobject
 	
 	
-	public LayerMask playermask;
+	public LayerMask playermask; //Layermask for the player
 	
-	[SerializeField] bool Valid = false;
+	[SerializeField] bool Valid = false; //This will let the enemyAI know if this hiding spot is valid or not
 	
 
 	// Start is called before the first frame update
 	void Start()
-    {
-		inrangecd = true;
+    	{
+		inrangecd = true; //This is just so the Ienumerator can run when it is called for the first time.
 
 	}
 
     // checks to see if the player is within the hiding spots overlapshpere.
     void Update()
 	{
-		Collider[] sphereinrange = Physics.OverlapSphere(this.gameObject.transform.position, sphereradius, playermask);
+		Collider[] sphereinrange = Physics.OverlapSphere(this.gameObject.transform.position, sphereradius, playermask); //Creatse a sphereoverlay that only registers items that have the Layermask that playermask is set to.
 		
-		foreach (Collider sphereingranges in sphereinrange)
+		foreach (Collider sphereingranges in sphereinrange) //gets each item within the overlapsphere that has the playermask layer on it and runs the code below.
 		{
 
-		
-			if (sphereingranges.CompareTag("Player"))
+			if (sphereingranges.CompareTag("Player")) //Just a double check to see if its the player, Makes sure you create a tag called "Player" and add it to the player.
 			{
 					inrange = true;
-					StartCoroutine(TurnOffInRadius());
+					StartCoroutine(TurnOffInRadius()); //Starts the TurnOffInRadius Coroutine.
 			}
 		}
 
@@ -64,18 +63,18 @@ public class HidingSpots : MonoBehaviour
 
 		//checks to see if the raycast has a hit and if the player is withing the overlap sphere, then calls the true ray function, if false runs the falseray function.
 
-		if (lasthit != null)
+		if (lasthit != null) //Just to make sure that last hit has a value attachted to it, this is to stop "Variable is null" errors and to check that the raycast actually hit cover
 		{
-			if (Valid != true)
+			if (Valid != true)//Checks if valid is not true, this is so the code isnt infinitly repeated over and over again.
 			{
 				if (inrange)
 				{
-					StartCoroutine(TrueRay());
+					StartCoroutine(TrueRay());// Starts the TrueRay Coroutine
 				}
 
 				else
 				{
-					StartCoroutine(FalseRay());
+					StartCoroutine(FalseRay());// Starts the FalseRay Coroutine
 				}
 			}
 
@@ -83,12 +82,12 @@ public class HidingSpots : MonoBehaviour
 		}
 		else
 		{
-			StartCoroutine(FalseRay());
+			StartCoroutine(FalseRay()); // Starts the FalseRay Coroutine since lasthit is equal to null
 		}
 
-		if (!inrange)
+		if (!inrange) //this is just to check if at anytime the player leaves the spherecast that it makes the spot invalid
 		{
-			StartCoroutine(FalseRay());
+			StartCoroutine(FalseRay());// Starts the FalseRay Coroutine
 		}
 
 		
@@ -96,57 +95,57 @@ public class HidingSpots : MonoBehaviour
 
     IEnumerator Raycast() // Runs a raycast and either returns a hit value, or null.
     {
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, RayDistance, CoverMask))
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, RayDistance, CoverMask)) //Runs a raycast that is only able to hit the CoverMask layermask
 		{
-			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * RayDistance, Color.yellow);
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * RayDistance, Color.yellow);//This is just to see the lines of the raycast
 
-				lasthit = hit;	
+				lasthit = hit;	//If the raycast does hit cover then saves its value to lasthit
 		}
 		else
 		{	
-				lasthit = null;	
+				lasthit = null;	//if the raycast doesnt hit cover then it makes lasthit value null
 		}
-		yield return new WaitForSeconds(0.1f);
+		yield return new WaitForSeconds(0.1f);//This is just so the code isnt run every frame
 	}
 
 	IEnumerator TrueRay() //if the conditions are right, it adds the hiding spots to the validspot list.
 	{
 
-		if (!manager.ValidSpots.Contains(this))
+		if (!manager.ValidSpots.Contains(this))//This is just to check that this spot isnt already set to Valid
 		{
-			manager.UpdateStatusToTrue(this);
-			Valid = true;
-			Debug.Log("Hitting Cover");
-			yield return new WaitForSeconds(5f);
+			manager.UpdateStatusToTrue(this); //Runs the UpdateStatusToTrue function in manager and runs this gameobject as a parameter
+			Valid = true; 
+			Debug.Log("Hitting Cover"); //Just so i can check if the code actually works
+			yield return new WaitForSeconds(5f);//This is so it doesn't run every frame
 		}
 		
 		
 	}
 	IEnumerator FalseRay() //if the conditions are not right, it removes the hiding spots to the validspot list.
 	{
-		if (manager.ValidSpots.Contains(this))
+		if (manager.ValidSpots.Contains(this))//This is just to check that this spot isnt already set to Invalid
 		{
-			manager.UpdateStatusToFalse(this);
+			manager.UpdateStatusToFalse(this);//Runs the UpdateStatusToFalse function in manager and runs this gameobject as a parameter
 			Valid = false;
-			Debug.Log("Hitting Not");
-			yield return new WaitForSeconds(5f);
+			Debug.Log("Hitting Not");//Just so i can check if the code actually works
+			yield return new WaitForSeconds(5f);//This is so it doesn't run every frame
 			
 		}
 		
 	}
-
+	//This is just to fix a bug i found.
 	IEnumerator TurnOffInRadius()
 	{
-		if (inrangecd)
+		if (inrangecd)// Checks to see if inrangecd is set to true, and so it doesnt get run every frame.
 		{
 			inrangecd = false;
-			yield return new WaitForSeconds(2f);
+			yield return new WaitForSeconds(2f);//Waits 2 seconds
 			inrange = false;
 			inrangecd = true;
 		}
 		
 	}
-
+	//This is just so the shphereoverlay is visable
 	private void OnDrawGizmos()    
 	{
 		Gizmos.color = Color.green;
